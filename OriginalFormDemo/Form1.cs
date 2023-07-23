3739207349e8dd1ab978d8ef08a1c2675f82137f
PavelSadykov <2439747@gmail.com>
2023-07-23 14:09:42 +0500

    Добавьте файлы проекта.

---
+++ OriginalFormDemo/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ComboBox = System.Windows.Forms.ComboBox;
+using Button = System.Windows.Forms.Button;
+
+namespace OriginalFormDemo
+{
+    public partial class Form1 : Form
+    {
+        Point pointStart; // точка для перемещения
+        private void addCloseButton() 
+        {
+            // Кнопка закрытия
+            Button buttonClose = new Button();
+            buttonClose.Location = new Point(this.Width / 3, this.Height / 3);
+            buttonClose.Text = "Х";
+            buttonClose.Click += buttonClose_Click;
+            this.Controls.Add(buttonClose); 
+        }
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void addChoiceForm()
+        {
+            ComboBox cmbChoice = new ComboBox();
+            cmbChoice.Location = new Point(this.Width/3, this.Height/3 + 30);
+            var items = new object[] { "normal", "ellipse", "polygon" };
+            cmbChoice.Items.AddRange(items);
+            cmbChoice.SelectedValueChanged += CmbChoice_SelectedValueChanged;
+            this.Controls.Add(cmbChoice);
+        }
+
+
+
+        private void CmbChoice_SelectedValueChanged(object sender, EventArgs e)
+        {
+            OrigWindow.setOriginalForm(this, ((ComboBox)sender).SelectedItem.ToString());
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            addCloseButton();
+            addChoiceForm();
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+            
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if (e.Button == MouseButtons.Left)
+            {
+                pointStart = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // если нажата левая кнопка мыши
+            if ((e.Button & MouseButtons.Left) != 0)
+            {
+                // получаем новую точку положения формы
+                Point deltaPos = new Point(e.X - pointStart.X, e.Y - pointStart.Y);
+                // устанавливаем положение формы
+                this.Location = new Point(this.Location.X + deltaPos.X,
+                  this.Location.Y + deltaPos.Y);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получение чисел из текстовых полей
+            if (!double.TryParse(textBox1.Text, out double number1) || !double.TryParse(textBox2.Text, out double number2))
+            {
+                MessageBox.Show("Введите корректные числа.");
+                return;
+            }
+
+            // Выполнение операции сложения
+            double result = number1 + number2;
+
+            // Отображение результата в поле вывода
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out double number1) || !double.TryParse(textBox2.Text, out double number2))
+            {
+                MessageBox.Show("Введите корректные числа.");
+                return;
+            }
+
+            // Выполнение операции сложения
+            double result = number1 - number2;
+
+            // Отображение результата в поле вывода
+            textBox3.Text = result.ToString();
+        }
+    
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        // Получение чисел из текстовых полей
+        if (!double.TryParse(textBox1.Text, out double number1) || !double.TryParse(textBox2.Text, out double number2))
+        {
+            MessageBox.Show("Введите корректные числа.");
+            return;
+        }
+        try
+        {
+            if (number2 == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль недопустимо");
+            }
+            // Выполнение операции деления
+            double result = number1 / number2;
+
+            // Отображение результата в поле вывода
+            textBox3.Text = result.ToString();
+
+        }
+        catch (DivideByZeroException ex)
+        {
+            //Обработка исключени деления на ноль
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        // Получение чисел из текстовых полей
+        if (!double.TryParse(textBox1.Text, out double number1) || !double.TryParse(textBox2.Text, out double number2))
+        {
+            MessageBox.Show("Введите корректные числа.");
+            return;
+        }
+
+        // Выполнение операции сложения
+        double result = number1 * number2;
+
+        // Отображение результата в поле вывода
+        textBox3.Text = result.ToString();
+    }
+    }
+}
---
+++ OriginalFormDemo/OrigWindow.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OriginalFormDemo
+{
+    internal class OrigWindow
+    {
+        static public void setOriginalForm (Form form, string _form = "normal")
+        {
+            
+            
+
+
+            // Создаём форму границ - объект GraphicsPath
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+           
+            if (_form == "polygon")
+            {
+
+                // Выключаем элементы управления формой
+                form.FormBorderStyle = FormBorderStyle.None;
+                form.BackColor = Color.Orange;
+              
+                Point[] myPoint = { 
+                    new Point(0,form.Height/3),// 1 точка
+                new Point(form.Width/3, 0),// 2 точка
+                new Point(2*form.Width/3, 0),// 3 точка
+                new Point(form.Width, form.Height/3),// 4 точка
+                new Point(form.Width, 2*form.Height/3),// 5 точка
+                new Point(2*form.Width/3, form.Height),// 6 точка
+                new Point(form.Width / 3, form.Height),// 7 точка
+                 new Point(0, 2*form.Height/3),// 8 точка
+                };
+                
+                myPath.AddPolygon(myPoint);
+                // Устанавливаем видимую область                           
+                Region myRegion = new Region(myPath);
+                form.Region = myRegion;
+            }
+            else
+            {
+                if (_form == "ellipse")
+                {
+                    // Выключаем элементы управления формой
+                    form.FormBorderStyle = FormBorderStyle.None;
+                    form.BackColor = Color.GreenYellow;
+                    // Эллипс с высотой и шириной формы
+                    myPath.AddEllipse(0, 0, form.Width, form.Height);
+                    // Устанавливаем видимую область                           
+                    Region myRegion = new Region(myPath);
+                    form.Region = myRegion;
+                }
+                else
+                {
+                    form.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+                    form.BackColor = Color.IndianRed;
+
+
+
+                }
+            }
+        }
+    }
+}
